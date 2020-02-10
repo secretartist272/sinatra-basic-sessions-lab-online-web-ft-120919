214bb27914770fa11a8517a8266c808f7e6194b9
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-10 14:24:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ class App < Sinatra::Base
   configure do
     enable :sessions
     set :session_secret, "cookies"
-end
+  end
   
   get '/' do
-    @session = session
+    erb :index
   end
+  
+  
   
 end
